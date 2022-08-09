ff2433d7f6b3891d5d7505d04de8d2e9a791f082
katerynamelnykova <102432364+katerynamelnykova@users.noreply.github.com>
2022-08-10 02:40:29 +0300

    [VOVA-15] Styled the project browser (#4)
    Using Google's Material Icons font.  See:
     - https://fonts.google.com/icons?icon.query=folder
     - https://developers.google.com/fonts/docs/material_icons
    
    * Created .gitignore
    * Styled the project-browser menu
    * Folder icon now opens and closes when you click on it
    * Folder icons are now cross-platform :)
    
    Co-authored-by: Kateryna Melnykova
    Co-authored-by: Will Braynen <wbraynen@gmail.com>

--- services.js
+++ services.js
@@ -24,7 +24,9 @@ function getProjects(elementID) {
 
                 // Each form: construct an array of html li elements
                 let form_html = `
-                            <li>${form_name}</li>
+                            <li class='dropdown-item'>&nbsp;&nbsp;&nbsp;&nbsp;
+                            <span class="material-symbols-outlined">description</span>
+                            ${form_name}</li>
                         `
                 form_items.push(form_html);
             });
@@ -34,9 +36,9 @@ function getProjects(elementID) {
 
             // Each project: construct an array of html li elememtns
             let project_html = `
-                        <li>
-                            ${project_name}
-                            <ul>
+                        <li class='project'>
+                            <span class='project-name dropdown-item'><span class='material-symbols-outlined'>folder</span> ${project_name}</span>
+                            <ul class='forms-list'>
                                 ${all_forms_in_this_project_html}
                             </ul>
                         </li>
@@ -45,9 +47,50 @@ function getProjects(elementID) {
             items.push(project_html);
         });
 
+
         // All the projects: construct the html (by concatenating the array of li elements together)
         let html = "<ul>" + items.join("") + "</ul>"
+        var selected_projects = []
 
         $(elementID).html(html)
+
+
+        var projects_tags = document.getElementsByClassName('project-name');
+
+        for (var i = 0; i < projects_tags.length; i++) {
+            projects_tags[i].addEventListener("click", function () {
+                if (this.closest('li').querySelector('ul').classList == "forms-list") {
+                    this.closest('li').querySelector('ul').classList.add("open");
+                    this.innerHTML = "<span class='material-symbols-outlined'>folder_open</span>" + this.innerHTML.slice(53);
+                    
+                }
+                else {
+                    this.closest('li').querySelector('ul').classList.remove("open");
+                    this.innerHTML = "<span class='material-symbols-outlined'>folder</span>" + this.innerHTML.slice(58);
+                }
+            });
+        }
+
+
+        var active_tags = document.getElementsByClassName('dropdown-item');
+
+        for (var i = 0; i < active_tags.length; i++) {
+            active_tags[i].addEventListener("click", function () {
+                for (i = 0; i < active_tags.length; i++) {
+                    if(active_tags[i].classList.contains('active')) {
+                        active_tags[i].classList.remove('active')
+                    }
+                }
+                this.classList.add("active");
+
+                document.addEventListener('click', (e) => {
+                    const clicked_on_list = e.composedPath().includes(document.querySelector('#projects'));
+                    const clicked_on_a = e.composedPath().includes(document.querySelector('.a'));
+                    if (!clicked_on_list && clicked_on_a) {
+                        this.classList.remove("active");
+                    }
+                })
+            });
+        }
     }
 }
